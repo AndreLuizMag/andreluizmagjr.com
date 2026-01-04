b0d62dbffb637562cbc2cdac0e912bd931d581d5
andre <andreluizmagjr@outlook.com>
2026-01-04 15:26:02 -0300

    Update articles visual

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,23 +4,23 @@ import { Header } from "@/components/Header";
 import "./styles.css";
 
 const inter = Inter({
-	variable: "--font-inter",
-	subsets: ["latin"],
+  variable: "--font-inter",
+  subsets: ["latin"],
 });
 
 const MainLayout = ({
-	children,
+  children,
 }: Readonly<{
-	children: ReactNode;
+  children: ReactNode;
 }>) => {
-	return (
-		<body
-			className={`${inter.variable} ds-flex flow-row-nw lg:flow-col-nw justify-start align-stretch gap-md sm:gap-xs p-md sm:p-xs`}
-		>
-			{children}
-			<Header />
-		</body>
-	);
+  return (
+    <body
+      className={`${inter.variable} ds-flex flow-row-nw lg:flow-col-nw justify-start align-stretch gap-md sm:gap-xs p-md sm:p-xs`}
+    >
+      {children}
+      <Header />
+    </body>
+  );
 };
 
 export default MainLayout;
