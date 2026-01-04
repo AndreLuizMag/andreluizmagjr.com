9b15c9179cc7115dc4c7ae471597a67299149326
andre <andreluizmagjr@outlook.com>
2026-01-04 15:27:29 -0300

    Lint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,31 +3,31 @@ import { Button } from "./Button";
 import "./styles.css";
 
 export const Header = () => {
-  return (
-    <header className="header p-xs radius-md">
-      <nav className="height-fill ds-flex flow-col-nw lg:flow-row-rev-nw justify-between align-center">
-        <div className="ds-flex-center flow-col-nw lg:flow-row-nw gap-xs">
-          <Button href="/" id="homepage" icon="House" label="Homepage" />
-          <Button
-            href="/articles"
-            id="articles"
-            icon="Newspaper"
-            label="Articles"
-          />
-        </div>
-        <div className="ds-flex-center flow-col-nw lg:flow-row-nw gap-xs">
-          {social.map((s: NavigationType) => (
-            <Button
-              key={s.id}
-              href={s.href}
-              id={s.id}
-              icon={s.iconName}
-              label={s.label}
-              openInNewTab
-            />
-          ))}
-        </div>
-      </nav>
-    </header>
-  );
+	return (
+		<header className="header p-xs radius-md">
+			<nav className="height-fill ds-flex flow-col-nw lg:flow-row-rev-nw justify-between align-center">
+				<div className="ds-flex-center flow-col-nw lg:flow-row-nw gap-xs">
+					<Button href="/" id="homepage" icon="House" label="Homepage" />
+					<Button
+						href="/articles"
+						id="articles"
+						icon="Newspaper"
+						label="Articles"
+					/>
+				</div>
+				<div className="ds-flex-center flow-col-nw lg:flow-row-nw gap-xs">
+					{social.map((s: NavigationType) => (
+						<Button
+							key={s.id}
+							href={s.href}
+							id={s.id}
+							icon={s.iconName}
+							label={s.label}
+							openInNewTab
+						/>
+					))}
+				</div>
+			</nav>
+		</header>
+	);
 };
